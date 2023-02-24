abf5e542fe9266ba665a7b89c30cec7e65ebc075
Lungisani Sithomo <lungisanisithomo@gmail.com>
2023-02-23 18:29:23 -0800

    ten ten

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_triangle - Prints a triangle of squares
@@ -16,17 +17,17 @@ void print_triangle(void)
 	}
 	else
 	{
-		for (x + 0; x < size; x++)
-		{
-			for (y = size - x; y > 1; y--)
-			{
-				_putchar(32);
-			}
-			for (z = 0; z <= x; z++)
-			{
-				_putchar(35);
-			}
-			_putchar('\n');
-		}
+	for (x + 0; x < size; x++)
+	{
+	for (y = size - x; y > 1; y--)
+	{
+		_putchar(32);
+	}
+	for (z = 0; z <= x; z++)
+	{
+		_putchar(35);
+	}
+		_putchar('\n');
+	}
 	}
 }
